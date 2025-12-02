cf755b528976e500d25c5ad56d36e1bd5de0caff
Mewyk <public@mewyk.com>
2025-12-01 21:47:13 -0800

    Update documentation structure and assets for CivitaiSharp
    - Added missing hrefs in the table of contents for guides
    - Updated logo references in docfx.json and added a new logo file
    - Introduced main.js for icon links to GitHub and NuGet

---
+++ Documentation/template/public/main.js
@@ -0,0 +1,14 @@
+export default {
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/Mewyk/CivitaiSharp',
+      title: 'GitHub'
+    },
+    {
+      icon: 'box-seam',
+      href: 'https://www.nuget.org/packages/CivitaiSharp.Core',
+      title: 'NuGet'
+    }
+  ]
+}
